c5817466471e3d1a6ce2ba501614cb33c529564c
Tyler H <47481160+Tyler-Henson@users.noreply.github.com>
2019-03-20 19:58:44 -0700

    Update Space Invaders clone cleanup

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,27 +5,19 @@ from settings import Settings
 
 def run_game():
     # initialize game and create a screen object.
-    pygame.init()
+    pygame.init()  # always use init() since its easier to always have it
     ai_settings = Settings()
     screen = pygame.display.set_mode(
         (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
-
-    # Set the background color.
-    # bg_color = (230, 230, 230)
 
     # Main loop of the game
     while True:
         # Watch for keyboard or mouse events
         event = pygame.event.wait()
         if event.type == pygame.QUIT:
-            break
-        # The recommended check for QUIT doesnt work:
-        '''for event in pygame.event.get():
-            if event.type == pygame.quit():
-                # pygame.display.quit()
-                # pygame.quit()
-                sys.exit()'''
+            sys.exit()
+            # break
 
         # Redraw the screen during each pass through the loop.
         screen.fill(ai_settings.bg_color)
