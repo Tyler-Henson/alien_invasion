ff045da3ac15078a76164ed75e7e8682d892da4f
Tyler H <47481160+Tyler-Henson@users.noreply.github.com>
2019-03-21 15:19:12 -0700

    Update Space Invaders clone

--- ship.py
+++ ship.py
@@ -6,15 +6,15 @@ class Ship:
     def __init__(self, screen):
         """Initialize the ship and set its starting position"""
         self.screen = screen
-
         # Load the ship image and get its rect.
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
         self.screen_rect = screen.get_rect()
 
         # Start each new ship at the bottom center of screen
-        self.rect.centerx = self.screen.rect.centerx
+        self.rect.center = self.screen_rect.center
         self.rect.bottom = self.screen_rect.bottom
+
 
     def blitme(self):
         """Draw the ship in its current location"""
