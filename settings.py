c9a27ef7b41a194063f80b241c8ad3d36d30510a
Tyler H <47481160+Tyler-Henson@users.noreply.github.com>
2019-03-31 15:16:42 -0700

    Working version of alien invasion

--- settings.py
+++ settings.py
@@ -18,10 +18,10 @@ class Settings:
 
         # Bullet settings
         self.bullet_speed_factor = 1
-        self.bullet_width = 3
+        self.bullet_width = 300  # test at 300
         self.bullet_height = 15
         self.bullet_color = 60, 60, 60
-        self.bullets_allowed = 3
+        self.bullets_allowed = 3  # test at 300
 
         # Alien settings
         self.alien_speed_factor = 1
