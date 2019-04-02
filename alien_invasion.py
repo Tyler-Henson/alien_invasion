88b0a405808ad30b466c1e9c68a56d075bd4d4d6
Tyler H <47481160+Tyler-Henson@users.noreply.github.com>
2019-04-02 16:12:18 -0700

    Working version of alien invasion

--- alien_invasion.py
+++ alien_invasion.py
@@ -8,8 +8,9 @@ from game_stats import GameStats
 from ship import Ship
 # from alien import Alien
 import game_functions as gf
+from button import Button
 
-  
+
 def run_game():
     # initialize game and create a screen object.
     pygame.init()  # always use init() since its easier to always have it
@@ -17,6 +18,9 @@ def run_game():
     screen = pygame.display.set_mode(
         (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
+
+    # Make the Play button
+    play_button = Button(ai_settings, screen, "Play")
 
     # Create an instance to store game stats.
     stats = GameStats(ai_settings)
@@ -31,14 +35,15 @@ def run_game():
 
     # Main loop of the game
     while True:
-        gf.check_events(ai_settings, screen, ship, bullets)
+        gf.check_events(ai_settings, screen, stats, play_button, ship, bullets)
         if stats.game_active:
             ship.update()
             #bullets.update()
             gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
             gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
 
-        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
+        gf.update_screen(ai_settings, screen, stats, ship, aliens, bullets,
+                         play_button,)
 
 
 if __name__ == '__main__':
