2f91f0ce9474818cdce3cffc6bb7144a5a2d028c
Tyler H <47481160+Tyler-Henson@users.noreply.github.com>
2019-04-02 16:21:09 -0700

    Working version of alien invasion

--- alien_invasion.py
+++ alien_invasion.py
@@ -37,9 +37,9 @@ def run_game():
     while True:
         gf.check_events(ai_settings, screen, stats, play_button, ship, aliens,
                         bullets)
+
         if stats.game_active:
             ship.update()
-            #bullets.update()
             gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
             gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
 
--- game_functions.py
+++ game_functions.py
@@ -31,7 +31,12 @@ def check_events(ai_settings, screen, stats, play_button, ship, aliens,
 def check_play_button(ai_settings, screen, stats, play_button, ship, aliens,
                       bullets, mouse_x, mouse_y):
     """Start a new game when the player clicks Play."""
-    if play_button.rect.collidepoint(mouse_x, mouse_y):
+    button_clicked = play_button.rect.collidepoint(mouse_x, mouse_y)
+    if button_clicked and not stats.game_active:
+        # Hide mouse cursor
+        pygame.mouse.set_visible(False)
+
+        # Reset
         stats.reset_stats()
         stats.game_active = True
 
@@ -178,6 +183,7 @@ def ship_hit(ai_settings, stats, screen, ship, aliens, bullets):
         sleep(0.5)
     else:
         stats.game_active = False
+        pygame.mouse.set_visible(True)
 
 
 def check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets):
