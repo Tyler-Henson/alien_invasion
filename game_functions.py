c952ff40d5beb2a32f57da552270b2fdd795150c
Tyler H <47481160+Tyler-Henson@users.noreply.github.com>
2019-04-07 18:16:57 -0700

    Working version of alien invasion

--- game_functions.py
+++ game_functions.py
@@ -135,7 +135,8 @@ def check_bullet_collisions(ai_settings, screen, stats, sb, ship, aliens,
     # If so, get rid of the bullet and the alien.
     collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
     if collisions:
-        stats.score += ai_settings.alien_points
+        for aliens in collisions.values():
+            stats.score += ai_settings.alien_points * len(aliens)
         sb.prep_score()
 
     if len(aliens) == 0:
--- settings.py
+++ settings.py
@@ -19,7 +19,7 @@ class Settings:
 
         # Bullet settings
         #self.bullet_speed_factor = 3  # 3 up from 1
-        self.bullet_width = 3  # 3
+        self.bullet_width = 300  # 3
         self.bullet_height = 15
         self.bullet_color = 60, 60, 60
         self.bullets_allowed = 3  # 3
@@ -32,6 +32,8 @@ class Settings:
 
         # How quickly the game speeds up
         self.speedup_scale = 1.1
+        # How quickly the alien point values increase
+        self.score_scale = 1.5
 
         self.initialize_dynamic_settings()
 
@@ -48,10 +50,13 @@ class Settings:
         self.alien_points = 50
 
     def increase_speed(self):
-        """Increase speed settings."""
+        """Increase speed settings and alien point values."""
         self.ship_speed_factor *= self.speedup_scale
         self.bullet_speed_factor *= self.speedup_scale
         self.alien_speed_factor *= self.speedup_scale
+
+        self.alien_points = int(self.alien_points * self.score_scale)
+        # print(self.alien_points)
 
 
 
